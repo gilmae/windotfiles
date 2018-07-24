2f7ac4434debfbb270eeec85b2264b6c27bab8a2
David Edgar <david.edgar@messagemedia.com.au>
2018-07-24 11:46:16 +1000

    Adding files directly in C:\Users\DavidEdgar

---
+++ .bashrc
@@ -0,0 +1,3 @@
+alias e=notepad++
+alias vs13='C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe'
+alias vs17='C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe'
---
+++ .cmdrc.bat
@@ -0,0 +1,3 @@
+doskey vs13="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" $*
+doskey vs17="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" $*
+doskey r="C:\Program Files\R\R-3.4.4\bin\rscript.exe" $*
