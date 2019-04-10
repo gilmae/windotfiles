a8ffb433acb115633aa023678379f8c1487a792c
gilmae <avocadia@fastmail.fm>
2019-04-10 14:09:03 +1000

    Some functions for finding and using the HEAD logs of a git repo. Can now display the branch in the prompt, and have a method for going back to the last branch

--- Documents/WindowsPowerShell/profile.ps1
+++ Documents/WindowsPowerShell/profile.ps1
@@ -19,10 +19,31 @@ Set-PSReadlineOption -BellStyle None
 function prompt {
 
   $ESC = [char]27
-  $Admin=''
-  $CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent() 
-  $principal = new-object System.Security.principal.windowsprincipal($CurrentUser)
-  if ($principal.IsInRole("Administrators")) { $Admin="ADMIN " }
+  # $Admin=''
+  # $CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent() 
+  # $principal = new-object System.Security.principal.windowsprincipal($CurrentUser)
+  # if ($principal.IsInRole("Administrators")) { $Admin="$ESC[31mAdmin$ESC[0m " }
+  "$(Get-Location) $esc[0;32m$(Get-Current-Branch)$esc[0m`r`n$('$ ' * ($nestedPromptLevel + 1))"
+}
 
-  "$ESC[31m$Admin$ESC[0m$(Get-Location)\`r`n$('$ ' * ($nestedPromptLevel + 1))$ESC[0m"
+function Find-Git-Dir {
+  $p = $pwd
+  while ($p -ne $Null) {
+    if (Join-Path -Path $p -ChildPath ".git" | Test-Path ) {
+        return $p
+    }
+    $p = (get-item $p).parent.FullName
+  }
+}
+
+function Get-Current-Branch {
+  Find-Git-Dir | Join-Path -ChildPath ".git\logs\HEAD" | gci | Get-Content | Select-String -Pattern "checkout: moving from .+ to (.+)" | % {$_.matches.groups[1].Value} | select-object -Last 1
+}
+
+function Get-Last-Branch {
+  Find-Git-Dir | Join-Path -ChildPath ".git\logs\HEAD" | gci | Get-Content | Select-String -Pattern "checkout: moving from .+ to (.+)" | % {$_.matches.groups[1].Value}  | Get-Unique | select-object -last 2 | select-object -first 1
+}
+
+function Git-Back {
+  iex "git checkout $(Get-Last-Branch)"
 }
